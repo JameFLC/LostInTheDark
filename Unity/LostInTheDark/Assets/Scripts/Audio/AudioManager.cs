63716dbb914713cf2b5bf62d64810ea3fbf4fdce
PaulCoret <115621137+PaulCoret@users.noreply.github.com>
2023-09-13 11:52:50 +0200

    AUDIO : INTEGRATION
    Modification de l'audio prefab : ajout de banks et events plus logique d'appel avec les fonctions en public.
    + load des banks dans l'awake
    + event test utilisant le death generic

--- Unity/LostInTheDark/Assets/Scripts/Audio/AudioManager.cs
+++ Unity/LostInTheDark/Assets/Scripts/Audio/AudioManager.cs
@@ -2,9 +2,33 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] AK.Wwise.Event _menuMusic = null;
-    [SerializeField] AK.Wwise.Event _Lvl1Music = null;
-    [SerializeField] AK.Wwise.Event _ambSound = null;
+    [Header("Banks")]
+    [SerializeField] AK.Wwise.Bank amb_bleeding_swamps = null;
+    [SerializeField] AK.Wwise.Bank amb_gate = null;
+    [SerializeField] AK.Wwise.Bank amb_howling_swamps = null;
+    [SerializeField] AK.Wwise.Bank amb_maldirach_swamps = null;
+    [SerializeField] AK.Wwise.Bank amb_moon_shadow = null;
+    [SerializeField] AK.Wwise.Bank amb_serpent_vigne = null;
+    [SerializeField] AK.Wwise.Bank SB_carnivist_choir = null;
+    [SerializeField] AK.Wwise.Bank SB_howling_swamps = null;
+    [SerializeField] AK.Wwise.Bank SB_reine_gouillante = null;
+    [SerializeField] AK.Wwise.Bank UI = null;
+
+
+    [Header("Events")]
+    [SerializeField] AK.Wwise.Event _Music = null;
+    [SerializeField] AK.Wwise.Event Play_amb_bleeding_swamps = null;
+    [SerializeField] AK.Wwise.Event Play_amb_gate = null;
+    [SerializeField] AK.Wwise.Event Play_amb_maldirach = null;
+    [SerializeField] AK.Wwise.Event Play_amb_moon_shadow = null;
+    [SerializeField] AK.Wwise.Event Play_amb_spawn = null;
+    [SerializeField] AK.Wwise.Event Play_howling_swamps = null;
+    [SerializeField] AK.Wwise.Event Play_serpent_vigne = null;
+    [SerializeField] AK.Wwise.Event Play_sound_bites_howling_swamps = null;
+    [SerializeField] AK.Wwise.Event Play_sound_bites_reine_gouillante = null;
+    [SerializeField] AK.Wwise.Event Play_ui_death_generic = null;
+    [SerializeField] AK.Wwise.Event Play_ui_death_reine_gouillante = null;
+    [SerializeField] AK.Wwise.Event _stopAll = null;
 
     private static AudioManager instance;
     public static AudioManager Instance => instance;
@@ -16,45 +40,113 @@ public class AudioManager : MonoBehaviour
         instance = this;
 
         DontDestroyOnLoad(this.gameObject);
+
+        //loading Banks
+        amb_bleeding_swamps.Load();
+        amb_gate.Load();
+        amb_howling_swamps.Load();
+        amb_maldirach_swamps.Load();
+        amb_moon_shadow.Load();
+        amb_serpent_vigne.Load();
+        SB_carnivist_choir.Load();
+        SB_howling_swamps.Load();
+        SB_reine_gouillante.Load();
+        UI.Load();
+        
+
+    }
+
+    private void Start()
+    {
+        UIDeathGeneric();
     }
 
     // Musics
-    public void MusicMenu(bool play)
+    public void MusicEvent(bool play)
     {
         if (play)
         {
-            _menuMusic.Post(gameObject);
+            _Music.Post(gameObject);
         }
         else
         {
-            _menuMusic.Stop(gameObject, 200);
+            _Music.Stop(gameObject, 200);
         }
 
     }
 
-    public void Lvl1Music(bool play)
+    // Ambs
+    public void AmbianceBleedingSwamps()
     {
-        if (play)
-        {
-            _Lvl1Music.Post(gameObject);
-        }
-        else
-        {
-            _Lvl1Music.Stop(gameObject, 200);
-        }
+        Play_amb_bleeding_swamps.Post(gameObject);
+
     }
 
-    // Amb
-    public void AmbSound(bool play)
+    public void AmbianceGate()
     {
-        if (play)
-        {
-            _ambSound.Post(gameObject);
-        }
+        Play_amb_gate.Post(gameObject);
 
-        else
-        {
-            _ambSound.Stop(gameObject, 200);
-        }
+    }
+
+    public void AmbianceMaldirach()
+    {
+        Play_amb_maldirach.Post(gameObject);
+
+    }
+
+    public void AmbianceMoonShadow()
+    {
+        Play_amb_moon_shadow.Post(gameObject);
+
+    }
+
+    public void AmbianceSpawn()
+    {
+        Play_amb_spawn.Post(gameObject);
+
+    }
+
+    public void AmbianceHowlingSwamps()
+    {
+        Play_howling_swamps.Post(gameObject);
+
+    }
+
+    public void AmbianceSerpentVigne()
+    {
+        Play_serpent_vigne.Post(gameObject);
+
+    }
+
+    //Sound Bites 
+    public void SBHowlingSwamps()
+    {
+        Play_sound_bites_howling_swamps.Post(gameObject);
+
+    }
+
+    public void SBReineGrouillante()
+    {
+        Play_sound_bites_reine_gouillante.Post(gameObject);
+
+    }
+
+    //UI
+    public void UIDeathGeneric()
+    {
+        Play_ui_death_generic.Post(gameObject);
+
+    }
+
+    public void UIDeathReineGrouillante()
+    {
+        Play_ui_death_reine_gouillante.Post(gameObject);
+
+    }
+    //utility
+    public void StopAll()
+    {
+
+        _stopAll.Post(gameObject);
     }
 }
--- Unity/LostInTheDark/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Unity/LostInTheDark/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,19 +13,45 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_AMB_BLEEDING_SWAMPS = 3516301799U;
+        static const AkUniqueID PLAY_AMB_GATE = 1087016108U;
+        static const AkUniqueID PLAY_AMB_MALDIRACH = 1197993866U;
+        static const AkUniqueID PLAY_AMB_MOON_SHADOW = 183445069U;
+        static const AkUniqueID PLAY_HOWLING_SWAMPS = 4216740504U;
+        static const AkUniqueID PLAY_SERPENT_VIGNE = 872436943U;
+        static const AkUniqueID PLAY_SOUND_BITES_HOWLING_SWAMPS = 2105663184U;
+        static const AkUniqueID PLAY_SOUND_BITES_REINE_GOUILLANTE = 1411513916U;
         static const AkUniqueID PLAY_TEST_SINE = 2929582682U;
+        static const AkUniqueID PLAY_UI_DEATH_GENERIC = 3490999669U;
+        static const AkUniqueID PLAY_UI_DEATH_REINE_GOUILLANTE = 1069451164U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMB_BLEEDING_SWAMPS = 1648313502U;
+        static const AkUniqueID AMB_GATE = 1075972715U;
+        static const AkUniqueID AMB_HOWLING_SWAMPS = 4239648128U;
+        static const AkUniqueID AMB_MALDIRACH_SWAMPS = 1504343901U;
+        static const AkUniqueID AMB_MOON_SHADOW = 2917579400U;
+        static const AkUniqueID AMB_SERPENT_VIGNE = 816784471U;
+        static const AkUniqueID SB_CARNIVIST_CHOIR = 2203198968U;
+        static const AkUniqueID SB_HOWLING_SWAMPS = 3605615547U;
+        static const AkUniqueID SB_REINE_GOUILLANTE = 2471527859U;
         static const AkUniqueID TESTBANK = 3291379323U;
+        static const AkUniqueID UI = 1551306167U;
     } // namespace BANKS
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBS = 3537656742U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID OS_REVERB_LARGE = 403037658U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
